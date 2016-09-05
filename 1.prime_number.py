4461be7ba609021df90b3473dc61ec6e0c3c171b
DaeShin <zhoupeng2014@gmail.com>
2016-09-05 14:43:02 +0800

    completed
    prime number completed
    
    Signed-off-by: DaeShin <zhoupeng2014@gmail.com>

--- 1.prime_number.py
+++ 1.prime_number.py
@@ -1,11 +1,25 @@
 # coding:utf-8
+import math
+
+
 def prime_solve(n):
     """
     output all the prime number from 1 to n
     :param lst: {index:the number}->{0:not a prime 1: a prime}
     :return:
     """
+    lst = []
+    for i in range(3, n + 1):
+        flag = True
+        j = 2
+        while j * j <= i:
+            if i % j == 0:
+                flag = False
+                break
+            j += 1
+        if flag:
+            lst.append(i)
+    return lst
 
+print prime_solve(10)
 
-
-    pass
