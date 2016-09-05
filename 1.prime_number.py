70a25a961414374f2bc8ce004be3d1ce698d1340
DaeShin <zhoupeng2014@gmail.com>
2016-09-05 13:48:18 +0800

    prime number
    Signed-off-by: DaeShin <zhoupeng2014@gmail.com>

---
+++ 1.prime_number.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+def prime_solve(n):
+    """
+    output all the prime number from 1 to n
+    :param lst: {index:the number}->{0:not a prime 1: a prime}
+    :return:
+    """
+
+
+
+    pass
